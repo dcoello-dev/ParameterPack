e8d80613f15168a32b05b8fc7e091423d8805059
David Coello Pulido <davidcoello@eprosima.com>
2022-09-08 09:27:00 +0200

    First Commit
    Signed-off-by: David Coello Pulido <davidcoello@eprosima.com>

---
+++ ParameterPack.hpp
@@ -0,0 +1,75 @@
+#ifndef PARAMTER_PACK_H_
+#define PARAMTER_PACK_H_
+
+#include <tuple>
+
+namespace pack {
+
+template <typename ... A>
+struct ParameterPack
+{
+    template<typename ... T>
+    struct Pack
+    {
+        template <typename ... W>
+        struct Unpacker
+        {
+            template<size_t rem, typename From>
+            struct unpack
+            {
+                using __tp = Unpacker<typename std::tuple_element<rem, From>::type, W...>;
+                using type = typename __tp::template unpack<rem - 1, From>::type;
+            };
+
+            template<typename From>
+            struct unpack<0, From>
+            {
+                using type = std::tuple<typename std::tuple_element<0, From>::type, W...>;
+            };
+        };
+
+        using tuple = std::tuple<T...>;
+        using sequence = std::index_sequence_for<T...>;
+        using Packer = ParameterPack<T...>;
+        static constexpr auto size = sizeof...(T);
+
+        template <std::size_t N>
+        using get = typename std::tuple_element<N, tuple>::type;
+    };
+
+    template <typename C, typename ... R>
+    struct push
+    {
+        using __tp = ParameterPack<A..., C>;
+        using pack = typename __tp::template concat<R...>::pack;
+    };
+
+    template <typename C>
+    struct push<C>
+    {
+        using __tp = ParameterPack<A..., C>;
+        using pack = Pack<A..., C>;
+    };
+
+    template <std::size_t N, typename C, typename ... R>
+    struct _remove
+    {
+        using pack = typename _remove<N - 1, R...>::pack;
+    };
+
+    template <typename C, typename ... R>
+    struct _remove<0, C, R...>
+    {
+        using pack = Pack<C, R...>;
+    };
+
+    template <std::size_t N>
+    struct pop
+    {
+        using pack = typename _remove<N, A...>::pack;
+    };
+};
+
+} // namespace pack
+
+#endif // PARAMTER_PACK_H_
