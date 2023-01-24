6a3890585838272b4f6f2e27d0e27ed71cb20647
David Coello Pulido <davidcoello@eprosima.com>
2023-01-24 21:27:51 +0100

    Add mix_t to mix two ParameterPacks
    Signed-off-by: David Coello Pulido <davidcoello@eprosima.com>

--- ParameterPack.hpp
+++ ParameterPack.hpp
@@ -7,6 +7,7 @@
 
 namespace meta {
 
+
 /**
  * @class ParameterPack
  *
@@ -76,7 +77,9 @@ struct ParameterPack
             struct __unpack_elem<0, _FROM>
             {
                 //! invoke BUILDER::Build with parameter pack, hold output value on type.
-                using type = typename BUILDER::Build<typename std::tuple_element<0, _FROM>::type, _BUFFER...>::type;
+                using type =
+                        typename BUILDER::template Build<typename std::tuple_element<0, _FROM>::type,
+                                _BUFFER...>::type;
             };
         };
 
@@ -103,11 +106,11 @@ struct ParameterPack
          * @tparam BUILDER output type builder.
          */
         template<typename BUILDER>
-        using unpack = typename __Unpacker<BUILDER>::__unpack_elem<size - 1, tuple>;
+        using unpack = typename __Unpacker<BUILDER>::template __unpack_elem<size - 1, tuple>;
 
         //! alias for unpacking output type.
         template<typename B>
-        using unpack_t = typename __Unpacker<B>::__unpack_elem<size - 1, tuple>::type;
+        using unpack_t = typename __Unpacker<B>::template __unpack_elem<size - 1, tuple>::type;
 
         /**
          * @class get
@@ -137,7 +140,7 @@ struct ParameterPack
          * @tparam E new element.
          */
         template <typename E>
-        using add = typename __packer::_push<E>;
+        using add = typename __packer::template _push<E>;
 
         /**
          * @class add_t
@@ -157,7 +160,7 @@ struct ParameterPack
          * @tparam N number of elements to remove from the pack.
          */
         template <size_t N>
-        using pop = typename __packer::_pop<N>;
+        using pop = typename __packer::template _pop<N>;
 
         /**
          * @class pop_t
@@ -168,8 +171,9 @@ struct ParameterPack
          */
         template <size_t N>
         using pop_t = typename pop<N>::type;
-
     };
+
+
 
     // PRIVATE internal packer functionallity
     template <typename C, typename ... R>
@@ -225,6 +229,156 @@ using TypePack = meta::ParameterPack<>::pack<Types...>;
  */
 template<typename T, T __v>
 using Constant = std::integral_constant<T, __v>;
+
+/**
+ * @class __Mix
+ *
+ * @brief Mix two ParameterPacks in to one.
+ *
+ * @details given a left LPP [A, B, C] and right RPP [1, 2, 3] generates an
+ *  output BUILDER::Build = [A1, A2, A3, B1, B2, B3, C1, C2, C3] of types OT.
+ *
+ * @warning private use only.
+ *
+ * @tparam _BUILDER final builder, need to have a nester Build method.
+ * @tparam _OT output type, neet to have 2 templatic parameters that will handle
+ *  combination of RPP and LPP element types.
+ * @tparam _LPP left input ParameterPack.
+ * @tparam _RPP right input ParameterPack.
+ */
+template<typename _BUILDER, typename _OT, typename _LPP, typename _RPP>
+struct __Mix
+{
+    /**
+     * @class __lpp_foreach
+     *
+     * @brief LPP foreach.
+     *
+     * @details for each of LPP elements doing for each of LPP elements for each one.
+     *
+     * @warning private use only.
+     *
+     * @tparam _HEAD current type element of LPP.
+     * @tparam _TAIL remaining elements of LPP.
+     */
+    template <typename _HEAD, typename ... _TAIL>
+    struct __lpp_foreach
+    {
+        /**
+         * @class __rpp_foreach
+         *
+         * @brief RPP foreach.
+         *
+         * @details foreach RPP build _OT type with LPP and RPP current elements.
+         *
+         * @warning private use only.
+         *
+         * @tparam __REM counter.
+         * @tparam S LPP.
+         */
+        template<size_t __REM, typename S>
+        struct __rpp_foreach
+        {
+            static constexpr size_t __s = S::template get_val<__REM>;
+            using _tp = typename _OT::template Type<_HEAD, __s>;
+            //! continue with rpp and add new element.
+            using type = typename __rpp_foreach<__REM - 1, S>::type::template add_t<_tp>;
+        };
+
+        //! stop condition when counter == 0.
+        template<typename S>
+        struct __rpp_foreach<0, S>
+        {
+            static constexpr size_t __s = S::template get_val<0>;
+            using _tp = typename _OT::template Type<_HEAD, __s>;
+            //! trigger lpp foreach and add last element.
+            using type = typename __lpp_foreach<_TAIL...>::type::template add_t<_tp>;
+        };
+
+        //! Built BUILDER::Build Type of OTs.
+        using type = typename __rpp_foreach<_RPP::size - 1, _RPP>::type;
+    };
+
+    // Stop condition for lpp foreach.
+    template <typename H>
+    struct __lpp_foreach<H>
+    {
+        template<size_t __REM, typename S>
+        struct __rpp_foreach
+        {
+            static constexpr size_t __s = S::template get_val<__REM>;
+            using _tp = typename _OT::template Type<H, __s>;
+            using type = typename __rpp_foreach<__REM - 1, S>::type::template add_t<_tp>;
+        };
+
+        //! Last LPP and RPP element creates ParameterPack.
+        template<typename S>
+        struct __rpp_foreach<0, S>
+        {
+            static constexpr size_t __s = S::template get_val<0>;
+            using _tp = typename _OT::template Type<H, __s>;
+            template <typename ... Types>
+            using __PP = ParameterPack<>::pack<Types...>;
+            //! Creates ParameterPack that is going to be pushed to the top.
+            using type = __PP<_tp>;
+        };
+
+        using type = typename __rpp_foreach<_RPP::size - 1, _RPP>::type;
+    };
+
+
+    /**
+     * @class __trigger
+     *
+     * @brief entry point to trigger mix.
+     *
+     * @details Mix process involves two nested unpack processes.
+     *
+     * @warning private use only.
+     */
+    struct __trigger
+    {
+        /**
+         * @class _NestedBuilder
+         *
+         * @brief Builder for high order unpack process.
+         *
+         * @warning private use only.
+         */
+        struct _NestedBuilder
+        {
+            template<typename ... Args>
+            struct Build
+            {
+                //! Calls internal __lpp_foreach with LPP parameter pack and triggers RPP unpack with given BUILDER
+                using type = typename __lpp_foreach<Args...>::type::template unpack_t<_BUILDER>;
+            };
+        };
+
+        //! triggers LPP unpack with _NestedBuilder
+        using type = typename _LPP::template unpack_t<_NestedBuilder>;
+    };
+
+    //! Output type.
+    using type = typename __trigger::type;
+};
+
+/**
+ * @class mix_t
+ *
+ * @brief mix RPP ParameterPack with LPP ParameterPack.
+ *
+ * @details given a left LPP [A, B, C] and right RPP [1, 2, 3] generates an
+ *  output BUILDER::Build = [A1, A2, A3, B1, B2, B3, C1, C2, C3] of types OT.
+ *
+ * @tparam _BUILDER final builder, need to have a nester Build method.
+ * @tparam _OT output type, neet to have 2 templatic parameters that will handle
+ *  combination of RPP and LPP element types.
+ * @tparam _LPP left input ParameterPack.
+ * @tparam _RPP right input ParameterPack.
+ */
+template<typename B, typename CT, typename _LPP, typename _RPP>
+using mix_t = typename __Mix<B, CT, _LPP, _RPP>::type;
 
 } // namespace meta
 
